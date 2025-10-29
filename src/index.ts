2280d97977a8cfbaca9ef687d1adf8d2d5337d74
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2025-10-29 20:10:34 -0300

    update using .env

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { User } from "./models/User";
 import { PaymentService } from "./services/PaymentService";
 import { StripeProvider } from "./providers/StripeProvider";
 import { PaypalProvider } from "./providers/PaypalProvider";
 
+const preferredPayment = process.env.PREFERRED_PAYMENT as "stripe" | "paypal";
+
 const user: User = {
   name: "Jackson Magnabosco",
-  preferredPayment: "paypal",
+  preferredPayment: preferredPayment,
 };
 
 let provider;
 
-if (user.preferredPayment === "paypal") {
+if (user.preferredPayment === "stripe") {
   provider = new StripeProvider();
 } else {
   provider = new PaypalProvider();
 }
+
+console.log(`User ${user.name} selected payment method: ${user.preferredPayment}`);
 
 const paymentService = new PaymentService(provider);
 paymentService.processPayment(150);
